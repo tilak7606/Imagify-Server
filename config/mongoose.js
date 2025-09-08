e50a01ab678e15b2813412db7f70733a18617d6d
tilak7606 <tilak113saini@gmail.com>
2025-09-08 17:36:52 +0530

    config change

--- config/mongoose.js
+++ config/mongoose.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
         console.log("Database Connected");
     })
 
-    await mongoose.connect(`${process.env.MONGODB_URI}/Imagify`)
+    await mongoose.connect(process.env.MONGODB_URI)
 }
 
 export  default connectDB;
